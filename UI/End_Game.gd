eabc7021ad8fe4b4d062b5ec7dcfef8e1127f67b
mckeondtanner <112101411+mckeondtanner@users.noreply.github.com>
2022-09-24 16:58:07 -0400

    project 2 base game finished

---
+++ UI/End_Game.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _ready():
+	$Label.text = "Thanks for playing! Your score was " + str(Global.score) + "."
+
+func _on_Play_pressed():
+	Global.reset()
+	var _scene = get_tree().changed_scene("res://Game.tscn")
